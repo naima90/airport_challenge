3c8a8d55082f44bf35e19eecdbbec086c6570979
Naima <naimaosman203@gmail.com>
2021-12-12 10:11:41 +0000

    instruct plane to take-off

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,10 @@
 class Airport
-
+    def initialize
+    @planes = []
+  end
   def land(plane)
+  end
+
+  def take_off(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it 'instruct a plane to land' do
     expect(subject).to respond_to :land
   end
+
+  it 'instruct a plane to take-off' do
+    expect(subject).to respond_to :take_off
+  end
 end
